a021cd9fe62d192e1f4ce086efe563008efdb318
John Meyer <meyer.1334@buckeyemail.osu.edu>
2021-04-18 12:19:47 -0400

    Created class for SuckUp power, as well as an Entity class for the Air particles

---
+++ KirbyGame/KirbyGame/Entities/Avatar/PowerUps/SuckUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System.Diagnostics;
+
+namespace KirbyGame
+{
+    public class SuckUp : IPowerUp
+    {
+        TextureDetails particle;
+        Avatar avatar;
+        List<AirParticle> particleEffects;
+        public SuckUp(Avatar avatar)
+        {
+            this.avatar = avatar;
+            this.particle = new TextureDetails(avatar.game.Content.Load<Texture2D>("avatar"), new Rectangle(49,115,2,4), 1);
+        }
+        public void Trigger()
+        {
+
+        }
+
+        public void ReleaseTrigger()
+        {
+
+        }
+
+    }
+
+    public class AirParticle : Entity
+    {
+        public AirParticle(Vector2 location, TextureDetails particle) : base(new Sprite(particle, location))
+        {
+        }
+        public override void HandleCollision(Collision collision, Entity collider)
+        {
+            
+        }
+    }
+}
