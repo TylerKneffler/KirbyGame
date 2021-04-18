a8f189bfe128525051e542abccc16f32ab793acd
John Meyer <meyer.1334@buckeyemail.osu.edu>
2021-04-18 12:30:30 -0400

    built initial suck animation (no real behavior). Still needs improvement

--- KirbyGame/KirbyGame/Entities/Avatar/PowerUps/SuckUp.cs
+++ KirbyGame/KirbyGame/Entities/Avatar/PowerUps/SuckUp.cs
@@ -14,19 +14,40 @@ namespace KirbyGame
         TextureDetails particle;
         Avatar avatar;
         List<AirParticle> particleEffects;
+        bool powerOn;
+        private int Timer;
         public SuckUp(Avatar avatar)
         {
             this.avatar = avatar;
             this.particle = new TextureDetails(avatar.game.Content.Load<Texture2D>("avatar"), new Rectangle(49,115,2,4), 1);
+            particleEffects = new List<AirParticle>();
+            powerOn = false;
+            Timer = 0;
         }
         public void Trigger()
         {
+            powerOn = true;
+        }
 
+        public void Update(GameTime gameTime)
+        {
+            if (powerOn)
+            {
+                Timer += gameTime.ElapsedGameTime.Milliseconds;
+                if(Timer > 250)
+                {
+                    Timer -= 250;
+                    //generate accurate random location
+                    Vector2 location = new Vector2(avatar.X,avatar.Y);
+                    particleEffects.Add(new AirParticle(location, particle));
+                }
+            }
         }
 
         public void ReleaseTrigger()
         {
-
+            particleEffects.Clear();
+            powerOn = false;
         }
 
     }
