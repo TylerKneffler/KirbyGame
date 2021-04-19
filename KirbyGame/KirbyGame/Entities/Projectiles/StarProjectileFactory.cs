4ca74ec6853a3646980a5876a4c3420cb7dad306
Jackson Jiang <jiang.1845@buckeyemail.osu.edu>
2021-04-19 12:07:40 -0400

    added star projectile

---
+++ KirbyGame/KirbyGame/Entities/Projectiles/StarProjectileFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace KirbyGame
+{
+    class StarProjectileFactory
+    {
+        private Game1 game;
+        private SoundEffect player;
+
+        public StarProjectileFactory(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public StarProjectile CreateStarProjectile(Vector2 location, int direction)
+        {
+            //this.player.Play();
+            return new StarProjectile(new Sprite(new TextureDetails(game.Content.Load<Texture2D>("starproj"), 4) , location), direction, game);
+        }
+
+    }
+}
