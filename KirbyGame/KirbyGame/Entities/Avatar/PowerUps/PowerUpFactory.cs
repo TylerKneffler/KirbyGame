048d8448a502651318cf39dc9c17a0beac92a92c
John Meyer <meyer.1334@buckeyemail.osu.edu>
2021-04-19 17:23:38 -0400

    eating enemies for powerup is working (at least for SirKibble)

---
+++ KirbyGame/KirbyGame/Entities/Avatar/PowerUps/PowerUpFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KirbyGame
+{
+    class PowerUpFactory
+    {
+        public static IPowerUp PowerUp(EnemyTest enemy, Avatar avatar)
+        {
+            IPowerUp ret = null;
+            if(enemy.enemytype is SuckSirKibbleTest)
+            {
+                ret = new Cutter(avatar);
+            }
+            return ret;
+        } 
+    }
+}
