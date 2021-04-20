9a2b85f32afb43d62fcdddb01634f3f21e0b5cd3
John Meyer <meyer.1334@buckeyemail.osu.edu>
2021-04-20 15:04:53 -0400

    Fixed lazer collision

--- KirbyGame/KirbyGame/Entities/Avatar/PowerUps/Lazer.cs
+++ KirbyGame/KirbyGame/Entities/Avatar/PowerUps/Lazer.cs
@@ -14,7 +14,7 @@ namespace KirbyGame
         public Avatar avatar;
         private LazerProjectileFactory factory;
         private bool powerOn;
-        private LazerProjectile lazer;
+        private List<LazerProjectile> lazer;
         private int cooldown;
         private int delay;
         private float tempX;
@@ -24,6 +24,7 @@ namespace KirbyGame
             this.avatar = avatar;
             factory = new LazerProjectileFactory(avatar.game);
             powerOn = false;
+            lazer = new List<LazerProjectile>();
             
         }
         public void Trigger()
@@ -47,17 +48,22 @@ namespace KirbyGame
         {
             if (this.avatar.Sprite.Direction == Sprite.eDirection.Left)
             {
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 16), (this.avatar.position.Y - 0)), 0, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 32), (this.avatar.position.Y - 16)), 0, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 48), (this.avatar.position.Y - 32)), 0, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 64), (this.avatar.position.Y - 48)), 0, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 16), (this.avatar.position.Y - 0)), 0, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 32), (this.avatar.position.Y - 16)), 0, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 48), (this.avatar.position.Y - 32)), 0, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X - 64), (this.avatar.position.Y - 48)), 0, false));
             }
             else
             {
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 48), (this.avatar.position.Y - 0)), 1, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 64), (this.avatar.position.Y - 16)), 1, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 80), (this.avatar.position.Y - 32)), 1, false));
-                avatar.game.levelLoader.list.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 96), (this.avatar.position.Y - 48)), 1, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 48), (this.avatar.position.Y - 0)), 1, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 64), (this.avatar.position.Y - 16)), 1, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 80), (this.avatar.position.Y - 32)), 1, false));
+                lazer.Add(factory.CreateLazerProjectile(new Vector2((this.avatar.position.X + 96), (this.avatar.position.Y - 48)), 1, false));
+            }
+            foreach (LazerProjectile beam in lazer)
+            {
+                avatar.game.map.Insert(beam);
+                avatar.game.levelLoader.list.Add(beam);
             }
         }
         public void Draw(SpriteBatch spritebatch)
