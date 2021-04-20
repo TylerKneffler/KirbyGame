d8ed6313a88749624e487e6d71a8d1dda1793fc4
John Meyer <meyer.1334@buckeyemail.osu.edu>
2021-04-20 19:20:55 -0400

    Damage re-added

--- KirbyGame/KirbyGame/Entities/Avatar/Avatar.cs
+++ KirbyGame/KirbyGame/Entities/Avatar/Avatar.cs
@@ -217,7 +217,7 @@ namespace KirbyGame
 
             if (collider is EnemyTest)
             {
-                
+                TakeDamage();
                 if (((EnemyTest)collider).enemytype is WhispyWoods || ((EnemyTest)collider).enemytype is DeadWhispyWoods)
                 {
                     if (CollisionDirection is Collision.Direction.Up)
